9fce63457d3106a0647e0c75d6411f0d2981088e
Mahika Singh <138625546+SinghMahika@users.noreply.github.com>
2026-08-02 23:59:59 +0530

    Update settings.py

--- backend/app/core/settings.py
+++ backend/app/core/settings.py
@@ -5,7 +5,6 @@ class Settings(BaseSettings):
     SNOWFLAKE_CORTEX_MODEL: str = "gpt-4o"
     SNOWFLAKE_ACCOUNT: str
     SNOWFLAKE_USER: str
-    SNOWFLAKE_PASSWORD: str
     SNOWFLAKE_TOKEN: str
     SNOWFLAKE_DATABASE: str
     SNOWFLAKE_SCHEMA: str
